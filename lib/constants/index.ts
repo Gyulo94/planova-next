c6be7b9cbedbfb9c7cad4fec4fb032e05330e0b6
Gyulo94 <qkrcjfrb112@gmail.com>
2025-09-03 07:38:39 +0900

    init

---
+++ lib/constants/index.ts
@@ -0,0 +1,5 @@
+export const SERVER_URL = process.env.SERVER_URL;
+export const APP_NAME = process.env.APP_NAME;
+export const APP_DESCRIPTION = process.env.APP_DESCRIPTION;
+export const LOGO = "/images/logo.png";
+export const DEFAULT_AVATAR = "/images/default-avatar.jpg";
