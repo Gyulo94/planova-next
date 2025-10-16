b2948576a4b48dcc18646a596aa44050dd46245a
Gyulo94 <qkrcjfrb112@gmail.com>
2025-10-16 14:05:18 +0900

    fix: 캘린더에서 새작업 추가기능 제거

--- components/task/calendar/event-card.tsx
+++ components/task/calendar/event-card.tsx
@@ -40,15 +40,15 @@ export default function EventCard({
   const router = useRouter();
 
   function onClick(e: MouseEvent<HTMLDivElement>) {
-    e.stopPropagation();
     router.push(`/workspaces/${workspaceId}/projects/${projectId}/tasks/${id}`);
   }
   return (
     <div className="px-2">
       <div
+        data-event-card
         onClick={onClick}
         className={cn(
-          "p-1.5 text-xs bg-accent text-primary border rounded-md border-l-4 flex flex-col gap-y-1.5 cursor-pointer hover:opacity-75 transition z-10",
+          "p-1.5 text-xs bg-accent text-primary border rounded-md border-l-4 flex flex-col gap-y-1.5 cursor-pointer hover:opacity-75 transition z-100",
           statusColorMap[status]
         )}
       >
